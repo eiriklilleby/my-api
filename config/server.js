5ade263f0e316528d189cae0c54e19e79bc580d9
eiriklilleby <eirik.lilleby@gmail.com>
2021-11-27 13:07:06 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7ec55b6003483ae92cdf7dd36d6cb83d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '362dcadd-c80f-46df-8edf-1fe83606cd1e'
+};
