113045b4c31a67b7c9dcaac76d6053baf351d5cc
eiriklilleby <eirik.lilleby@gmail.com>
2021-12-16 12:33:06 +0100

    Changes to my-project noted

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 8082),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '7ec55b6003483ae92cdf7dd36d6cb83d'),
+      secret: env("ADMIN_JWT_SECRET", "7ec55b6003483ae92cdf7dd36d6cb83d"),
     },
   },
 });
